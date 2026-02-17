2eab083151391461b707bfcf4c3e8895c80864cf
pushou <pushou@nowhere>
2026-02-17 18:51:10 +0100

    first commit

--- fix-gns3.sh
+++ fix-gns3.sh
@@ -3,4 +3,4 @@ pip3 uninstall gns-server gns3-gui -y
 python3 -m venv /home/test/gns3env
 source /home/test/gns3env/bin/activate
 pip3 install pip --upgrade
-pip3 install gns3-{server,gui}==2.2.56 PyQt5
+pip3 install gns3-{server,gui}==2.2.53 PyQt5
