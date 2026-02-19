29ef47c8bb6cd50cfc1ce062b1c5fd7fb4794c3b
pushou <pushou@nowhere>
2026-02-19 12:11:08 +0100

    first commit

--- fix-gns3.sh
+++ fix-gns3.sh
@@ -1,9 +1,10 @@
 sudo apt update 
-sudo rm -f /usr/local/bin/gns3server
+sudo rm -f /usr/local/bin/gns3*
+sudo mkdir -p /home/gns3env
 sudo  apt-get -y install python3-venv busybox-static
 pip3 uninstall gns-server gns3-gui -y
-python3 -m venv /home/test/gns3env
-chown -R test:test /home/test/gns3env
-source /home/test/gns3env/bin/activate
+sudo chown -R test:test  /home/gns3env
+python3 -m venv /home/gns3env
+source /home/gns3env/bin/activate
 pip3 install pip --upgrade
 pip3 install gns3-{server,gui}==2.2.53 PyQt5
